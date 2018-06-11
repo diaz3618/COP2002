235dd5eaf5ef9ac55b62142388b0c562b6bba380
ALIENWARE14\Daniel <daniel.diaz.stg@gmail.com>
2018-06-11 12:52:27 -0400

    validation
    added a validation function

--- Mod4.py
+++ Mod4.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 
-def isPrime(a):
-	if a <= 2:
+def validate(num):
+        if num <= 0:
+                print("\nNumber must be bigger than 0.\n\n")
+                main()
+
+def isPrime(num):
+	if num <= 2:
 		return True
 	
-	for i in range(2, a):
-		if a % i * i == 0:
+	for i in range(2, num):
+		if num % i * i == 0:
 			return False
 	return True
 
-def factor(a):
+def factor(num):
 	print("The factor of your numbers are:")
 	
 	# Factor number
-	for i in range(1, a+1):
-		if a % i == 0:
+	for i in range(1, num+1):
+		if num % i == 0:
 			print(str(i))
 	
 	# Check if variable "a" is prime
-	if isPrime(a) == True:
-		print(str(a) + " is a prime number.")
+	if isPrime(num) == True:
+		print(str(num) + " is a prime number.")
 	else:
-		print(str(a) + " is not a prime number.")
+		print(str(num) + " is not a prime number.")
 
 def main():
 	print("Prime Number Checker\n")
@@ -30,7 +35,7 @@ def main():
 	while choice.lower() == "y":
 		# Ask for a number to factor
 		num = int(input("Please enter a integer between 1 and 5,000: "))
-		
+		validate(num)
 		# Factor function
 		factor(num)
 		
