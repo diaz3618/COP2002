b2b09d80b054527d93a28a59c6533b95b02fb044
Daniel Diaz Santiago <daniel.diaz.stg@gmail.com>
2018-06-21 08:57:06 -0400

    Comments
    added comments and some small changes.

--- Inventory.py
+++ Inventory.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-
+ 
 def main():
+        print("Inventory app\n")
         menu()
         
         # Continue loop, or break
         again = "y"
         print()
         while again.lower() != "n" or again.lower() != "y":
-            again = str(input("\nAdd another SKU? (y/n): "))
             print()
             if again.lower() == "n":
                 print("\nBye!")
@@ -16,20 +16,34 @@ def main():
                 inv()
                 write("", True)
             print("[-] Invalid choice...\n")
-
+            again = str(input("\nAdd another SKU? (y/n): "))
+ 
 def menu():
-        print("Inventory app\n\nChoose an option")
+        rmData()
+        """
+        Function asks the user to choose 1 of 2
+        options: "w" to add to file, and "r" to read.
+        Option "r" outputs file content, while "w" breaks
+        out of the loop.
+        """
+        print("\nChoose an option:")
+        choice = str(input("(r)Read file, (w)Write to file: "))
         
-        option = str(input("(r)Read file, (a)Add to file: "))
-        while option.lower() != "r" or option.lower() != "a":
-            print()
-            if option.lower() == "r":
+        while choice.lower() != "r" or choice.lower() != "w":
+            if choice.lower() == "r":
                 openFile()
-                option = str(input("(r)Read file, (a)Add to file: "))
-            elif option.lower() == "a":
-                w_or_a()
-                inv()
-            print("[-] Invalid choice...\n")
+            elif choice.lower() == "w":
+            	break
+            else:
+            	print("[-] Invalid choice...\n")
+            	
+            choice = str(input("(r)Read file, (w)Write to file: "))
+
+def rmData():
+        rm = str(input("Clear list.txt? (y/any key): "))
+        if rm.lower() == "y":
+        	write("", False)
+        	print("[+]Cleared\n")
             
 def inv():
         # Initialize
@@ -37,60 +51,52 @@ def inv():
         length = 1
         lf = "n"
 
-        # Ask once
+        """
+        Ask once, SKU might be
+        counted by linear feet.
+        """
         sku  = str((input("SKU: ")))
         lf = input("LF? (y/default = n): ")
 
+        """
+        While loop is used here to keep
+        adding to the overall amount.
+        *The loop ends when 0 is entered.
+        """
         while True:
                 # Linear Feet?
                 if lf.lower() == "y":
                         length = int(input("Length: "))
-
+ 
                 # Amount input
                 amount = int(input("Amount: "))
                 if amount == 0:
                         write("SKU: " + str(sku) + "\tQty. " + str(total) + "\n")
                         return False
-
+ 
                 total += mult(length, amount)
                 print("\tSKU: " + sku)
                 print("\tTotal: " + str(total))
                 
-
+ 
 def write(info, writeNew = True):
+        """
+        If "writeNew" == False,
+        write over the file (erase it).
+        """
         if writeNew == False:
                 f = open("list.txt", "w+")
         f = open("list.txt", "a+")
         f.write(info)
         f.close()
-
+ 
 def openFile():
         f = open("list.txt", "r")
-
+ 
         if f.mode == "r":
                 listFile = f.read()
                 print(listFile)
-        
-        
-
-def w_or_a():
-        # Write new or append to file
-        fileOption = input("(w)Write new file, (a)Append to file, (b)Back?: ")
-
-        while fileOption.lower() != "w" or fileOption.lower() != "a":
-                if fileOption.lower() == "w":
-                        write("",False)
-                        break
-                elif fileOption.lower() == "a":
-                        write("")
-                        break
-                elif fileOption.lower() == "b":
-                        menu()
-                        break
-                else:
-                        print("[-] Invalid choice...\n")
-                        fileOption = input("(w)Write new file, (a)Append to file?: ")
-
+ 
 def mult(length, amnt):
         total = length * amnt
         return total
